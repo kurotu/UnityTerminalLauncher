be29cbf2a37e8bca075a18e9552da86d78fd4ec1
kurotu <kurotu@gmail.com>
2021-08-21 17:21:15 +0900

    Remove unused using directives

--- Editor/TerminalSettingsProvider.cs
+++ Editor/TerminalSettingsProvider.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
-using UnityEngine;
-using UnityEngine.Experimental.UIElements;
+﻿using UnityEditor;
 using System;
 using System.Linq;
 
